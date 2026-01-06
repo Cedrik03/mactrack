0a9c9b00089b5f904062ee2edc5c35adc18b8661
Amanda Grip <amanda.grip@stud.sti.se>
2026-01-06 13:53:25 +0100

    small fixes with functions

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,9 +1,16 @@
 const API_URL = "http://localhost:8080/api/meals"; //backend-API's adress
 
+document.getElementById('addMealButton').addEventListener('click', addMeal);
+
 function addMeal() {
     const name = document.getElementById("name").value;
     const calories = document.getElementById("calories").value;
     const mealType = document.getElementById("mealType").value;
+
+    if(!name || !calories || !mealType) {
+        alert("Fyll i alla fält tack!");
+        return;
+    }
 
     fetch(API_URL, {
         method: "POST",
@@ -20,14 +27,27 @@ function addMeal() {
         .then(data => {
             document.getElementById("result").innerText =
                 "Tillagt: " + data.name + " (" + data.calories + " kcal)";
+
+            document.getElementById("name").value = '';
+            document.getElementById("calories").value = '';
         })
+
 }
 
 function getMealTotal(mealType) {
-    fetch(API_URL + "/total/" + mealType)
+
+    const url = mealType ? `${API_URL}/total/${mealType}` : `${API_URL}/total`;
+
+    fetch(url)
         .then(response => response.text())
         .then(total => {
-            document.getElementById("result").innerText =
-                "Totalt idag : " + total + " kcal";
+                document.getElementById("result").innerText =
+                    mealType
+                        ? `Totalt för ${mealType.toLowerCase()}: ${total} kcal`
+                        : `Totalt idag: ${total} kcal`;
         })
+        .catch(err => {
+            console.error(err);
+            alert("Något gick fel vid hämtning av totalsumma!");
+        });
 }
